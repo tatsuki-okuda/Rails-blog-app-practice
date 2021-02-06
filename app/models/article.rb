d1ab14a41bb1122e28d47066a80822f4a4e44ea0
tatsuki okuda <t-okuda@lotus-gr.co,jp>
2021-02-06 11:33:34 +0900

    コードの最適化 部分テンプレで簡略化

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+
+
+    def display_created_at
+        #  18n は国際化 
+        #  ja.ymlのデフォルト設定が反映される  
+         I18n.l(self.created_at, format: :default) 
+    end
+    
 end
