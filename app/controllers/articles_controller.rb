ea24f36a980f01bf2449d943fca6ad3184077929
tatsuki okuda <t-okuda@lotus-gr.co,jp>
2021-02-11 12:29:50 +0900

    userの下にarticleが来るように紐付け直し。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
     # 各アクションの前に発火するアクションを追加できる
-    before_action :set_article, only: [:show, :edit, :update,]
+    # before_action :set_article, only: [:show, :edit, :update,]
+    before_action :set_article, only: [:show]
+    before_action :set_current_article, only:[:edit, :update]
     # diviseが用意しているメソッド
     # ログインしてい状態だと昨日が使えなくなる
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
@@ -26,11 +28,14 @@ class ArticlesController < ApplicationController
 
     def  new
         # フォームに渡す中身のない側だけのインスアンス変数を作る
-        @article = Article.new()
+        # @article = Article.new()
+        # ログインしているユーザーの情報を取得する
+        @article = current_user.articles.build
     end
 
     def create
-        @article = Article.new(article_params)
+        # @article = Article.new(article_params)
+        @article = current_user.articles.build(article_params)
         # データを保存する
         # データが保存できた時とできなかった時とで処理を分ける
         # 保存に成功したらarticleのページに飛ばす
@@ -45,10 +50,15 @@ class ArticlesController < ApplicationController
 
     def edit
         # @article = Article.find(params[:id])
+
+        # ログインしているユーザーの記事から探し出す
+        #current_userではなくArticlesだと全体の生地からの検索になるので、他のユーザーの記事も含んでしまう
+        # @article = current_user.articles.find(params[:id])
     end
 
     def update
         # @article = Article.find(params[:id])
+        # @article = current_user.articles.find(params[:id])
         if @article.update(article_params)
             redirect_to article_path(@article), notice: '更新できました'
         else
@@ -60,7 +70,11 @@ class ArticlesController < ApplicationController
     def destroy
         # railsにおいてインスタンス変数は特別な意味を持ち。viewに表示する役割がある。
         # destroyにおいてはviewに表示させる必要がないので、インスタンス変数にはせずにアクションないの変数に留めておく。
-        article = Article.find(params[:id])
+        # article = Article.find(params[:id])
+
+        # Articleだと全部の生地から削除できるので、ログインユーザーに限らせる
+        article = current_user.articles.find(params[:id])
+
         #  !をつけるときに削除されなかった時に例外処理をくらえられる。
         # 削除の場合は削除前と後で整合性を取らないといけない。
         article.destroy!
@@ -83,5 +97,10 @@ class ArticlesController < ApplicationController
         # ただの変数の定義だと値を再利用できなくなる
         @article = Article.find(params[:id])
     end
+
+    def set_current_article
+        @article = current_user.articles.find(params[:id])
+    end
+    
 
 end
--- db/migrate/20210130030905_create_articles.rb
+++ db/migrate/20210130030905_create_articles.rb
@@ -2,8 +2,12 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
 
-      t.string :title
-      t.text :content
+      # userに紐付ける
+      # null: falseでカラムに絶対に値が入っていないといけない制約を加える
+      # userにアーティクルを絶対的に紐づける
+      t.references :user, null: false
+      t.string :title, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
